0e5d57a3558631015101afb1efdab436eb8701f4
Faiza-Latif <faiza@outlook.pt>
2022-10-04 11:40:37 +0100

    Two ways of adding dynamic classes

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,9 +25,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={{borderColor: !isValid ? 'red' : ''}} type="text" onChange={goalInputChangeHandler} />
+      {/*<div className={isValid ? 'form-control' : 'form-control invalid'} >*/}
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label> Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
